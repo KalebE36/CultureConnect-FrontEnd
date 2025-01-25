bfb42d1536a4600a63916c28955a2e0a22360adf
AntonCSalvador <antoncsalvador@gmail.com>
2025-01-25 15:05:02 -0500

    fixed env

--- frontend/src/config/firebaseConfig.ts
+++ frontend/src/config/firebaseConfig.ts
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMRsSBJKHwEIevZxNQaBD0fwlHf-KCjpI",
-  authDomain: "cultureconnectv2.firebaseapp.com",
-  projectId: "cultureconnectv2",
-  storageBucket: "cultureconnectv2.firebasestorage.app",
-  messagingSenderId: "197753224241",
-  appId: "1:197753224241:web:bf6d80d6d8ab56514d2910"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
