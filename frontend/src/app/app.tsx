8784e75569cd5dbd1d7c98c2aa542955028e6841
kalebE36 <kaleb.ellington33@gmail.com>
2025-01-25 13:34:35 -0500

    auth done

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginScreen from '../features/login/screens/Login';
+import LoginScreen from '../features/auth/screens/Login';
 import HomeScreen from '../features/home/screens/Home';
 import Camera from '../features/cam/screens/Camera';
 import ProtectedRoute from '../features/auth/components/ProtectedRoute';
@@ -8,8 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/login" element={<ProtectedRoute><LoginScreen/></ProtectedRoute>} />
+            <Route path="/" element={<ProtectedRoute><HomeScreen /></ProtectedRoute>} />
+            <Route path="/login" element={<LoginScreen/>} />
             <Route path="/camera" element={<ProtectedRoute><Camera/></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
